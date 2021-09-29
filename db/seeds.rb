9f488f77d245df5983915314bafa6f3e7986a327
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-09-29 09:50:02 -0400

    Update seeds.rb to include the base case 'Select' user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 puts "🌱 Seeding users..."
-userx01 = User.create!(user_name: 'Jago')
-userx02 = User.create!(user_name: 'Stella')
+userx01 = User.create!(user_name: 'Select')
+userx02 = User.create!(user_name: 'Jago')
+userx03 = User.create!(user_name: 'Stella')
  
 puts "🌱 Seeding types..."
 typex01 = Type.create!(type_name: 'coding')
@@ -8,17 +9,17 @@ typex02 = Type.create!(type_name: 'eating')
 typex03 = Type.create!(type_name: 'misc.')
 
 puts "🌱 Seeding todos..."
-Todo.create!(user_id: userx01.id, type_id: typex02.id, todo_name: "go to the store")
 Todo.create!(user_id: userx02.id, type_id: typex02.id, todo_name: "go to the store")
-Todo.create!(user_id: userx01.id, type_id: typex01.id, todo_name: "complete feature a on project")
-Todo.create!(user_id: userx01.id, type_id: typex01.id, todo_name: "complete feature bb on project")
-Todo.create!(user_id: userx02.id, type_id: typex01.id, todo_name: "complete feature ccc on project")
-Todo.create!(user_id: userx02.id, type_id: typex01.id, todo_name: "complete feature dddd on project")
-Todo.create!(user_id: userx01.id, type_id: typex02.id, todo_name: "wash dishes")
-Todo.create!(user_id: userx01.id, type_id: typex02.id, todo_name: "take out the trash")
-Todo.create!(user_id: userx01.id, type_id: typex03.id, todo_name: "complete morning routine")
+Todo.create!(user_id: userx03.id, type_id: typex02.id, todo_name: "go to the store")
+Todo.create!(user_id: userx02.id, type_id: typex01.id, todo_name: "complete feature a on project")
+Todo.create!(user_id: userx02.id, type_id: typex01.id, todo_name: "complete feature bb on project")
+Todo.create!(user_id: userx03.id, type_id: typex01.id, todo_name: "complete feature ccc on project")
+Todo.create!(user_id: userx03.id, type_id: typex01.id, todo_name: "complete feature dddd on project")
 Todo.create!(user_id: userx02.id, type_id: typex02.id, todo_name: "wash dishes")
 Todo.create!(user_id: userx02.id, type_id: typex02.id, todo_name: "take out the trash")
 Todo.create!(user_id: userx02.id, type_id: typex03.id, todo_name: "complete morning routine")
+Todo.create!(user_id: userx03.id, type_id: typex02.id, todo_name: "wash dishes")
+Todo.create!(user_id: userx03.id, type_id: typex02.id, todo_name: "take out the trash")
+Todo.create!(user_id: userx03.id, type_id: typex03.id, todo_name: "complete morning routine")
 
 puts "✅ Done seeding!"

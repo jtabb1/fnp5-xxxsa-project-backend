1f946b5f6d8c74a5fdc10a97c93c69a2a49ecef7
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-10-04 13:33:31 -0400

    Minor comment change

--- db/migrate/20210927000001_create_users.rb
+++ db/migrate/20210927000001_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :username
-      #include below for auth:
+      # Draft include below for auth:
       t.string :password_digest
 
       t.timestamps

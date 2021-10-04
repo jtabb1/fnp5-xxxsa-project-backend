d5674c0203d3940bcd28deb05e837c461cdecc7e
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-10-04 14:22:52 -0400

    Modify user model for auth per Flatiron example

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   has_many :todos, dependent: :destroy
   has_many :types, through: :todos
 
-  validates :username, presence: true
+  #Draft modifications for auth below:
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: true
 end

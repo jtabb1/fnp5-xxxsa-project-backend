0b643c61c747d42c946870184f8a7e44afa4454e
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-10-04 12:41:34 -0400

    Drafted routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   resources :todos, only: [:index, :show, :update, :create, :destroy]
   resources :types, only: [:index]
   resources :users, only: [:index, :show, :create, :update, :destroy]
+
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end

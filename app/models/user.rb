ff7379983f816a55c11111ff527d5bd38464d67a
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-10-08 13:49:14 -0400

    Remove unnecessary comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,11 @@ class User < ApplicationRecord
   has_many :common_todos, dependent: :destroy
   has_many :types, through: :common_todos
 
-  #Draft modifications for auth below: 
   has_secure_password
-
-  # def self.create_with_starter_types_and_todos(username, password, password_confirmation)
-  #   user = User.create(username: username, password: password, password_confirmation: password_confirmation)
-  # end
 
   def self.create_with_starter_types_and_todos(args)
     user = User.create(args)
-    # puts StarterType.all.inspect
-    # puts StarterType.all
     StarterTypedTodo.all.each do |obj|
-      # puts obj.inspect
-      # puts obj
-      # puts obj.type_name
       type = Type.find_or_create_by(user_id: user.id, type_name: obj.type_name)
       CommonTodo.find_or_create_by(user_id: user.id, type_id: type.id, todo_name: obj.todo_name, todo_notes: obj.todo_notes)
     end

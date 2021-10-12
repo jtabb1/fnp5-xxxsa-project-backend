31a73f73257a345d51c0e031739b1da9659bb15f
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-10-12 06:07:40 -0400

    Add fields to serializer and change package.json for heroku

--- app/serializers/common_todos_serializer.rb
+++ app/serializers/common_todos_serializer.rb
@@ -1,5 +1,5 @@
 class CommonTodoSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :type_id
+  attributes :id, :user_id, :type_id, :to_display, :todo_name
   has_one :user
   has_one :type
 end
--- app/serializers/todo_serializer.rb
+++ app/serializers/todo_serializer.rb
@@ -1,5 +1,5 @@
 class TodoSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :type_id
+  attributes :id, :user_id, :type_id, :todo_name
   has_one :user
   has_one :type
 end
